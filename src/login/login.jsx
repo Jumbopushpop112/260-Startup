5d14980f4cbab6d063dc4e0c7bc7ebfc26691653
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-02-21 14:54:32 -0700

    Working on login setup

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,6 +2,9 @@ import React,{ useEffect, useState } from 'react';
 export function Login() { 
   const [time, setTime] = useState('');
   const [date, setDate] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState(''); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
   useEffect(() => {
     function updateTime() {
@@ -12,28 +15,53 @@ export function Login() {
 
     updateTime(); // run immediately
     const interval = setInterval(updateTime, 1000);
-
+   
     return () => clearInterval(interval); // cleanup
   }, []); 
+   function Login(event){ 
+      event.preventDefault();
+      accountExists = true; 
+      if(localStorage.getItem(username) === null){
+        console.log("Username does not exist. Try again.");  
+      }
+      console.log("Username:", username);
+      console.log("Password:", password);   
+      setIsLoggedIn(true);   
+    }
+    function createAccount(event){
+      const users = JSON.parse(localStorage.getItem("Users")) || [];  
+      const isIncluded = users.some(
+        (user) => user.username === username
+      );  
+      if(isIncluded){ 
+        console.log("Oops! Username is taken"); 
+        alert("Username is taken!"); 
+      }else{
+        users.push({username, password});  
+      localStorage.setItem("Users",JSON.stringify(users));
+      }
+       
+    }
   return ( 
       <main className="container-fluid bg-secondary text-center">
       <div id="login"> 
-          <h1>Welcome to Slime!</h1>  
-      <form method="get" action="play.html">
+          {isLoggedIn && <h1>Welcome, {username}!</h1>}
+          {!isLoggedIn && <h1>Welcome to Slime!</h1>}     
+      <form onSubmit={Login}> 
         <div>
           <span style={{fontSize: '20px'}}>@</span>  
-          <input type="text" placeholder="Username" className="btn btn-outline-primary"/>  
+          <input type="text" placeholder="Username" className="btn btn-outline-primary" onChange={(e) => setUsername(e.target.value)}/>   
         </div>
         <br /> 
         <div>
           <span>🔒</span>
-          <input type="password" placeholder="Password" className="btn btn-outline-primary" /> 
+          <input type="password" placeholder="Password" className="btn btn-outline-primary" onChange={(e) => setPassword(e.target.value)} /> 
         </div> 
         <br /> 
-        <button type="submit">Login</button>  
-        <button type="submit">Create Account</button>
+        <button type="button" onClick={(event) => Login(event)}>Login</button>       
+        <button type="button" onClick={(event) => createAccount(event)}>Create Account</button>
       </form>
-      </div>
+      </div> 
     <footer id="navigationList"> 
       <hr />
       <span className="text-reset"><a href="mailto:matthewhart800@gmail.com">Contact Matthew Hart</a></span>   
