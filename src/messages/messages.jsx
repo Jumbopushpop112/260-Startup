78d3ae1427e1fa73a825ff995a14c6a425874573
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-02-25 19:18:23 -0700

    Message is displayed via alert

--- src/messages/messages.jsx
+++ src/messages/messages.jsx
@@ -1,14 +1,21 @@
 import React from 'react'; 
+import { useEffect, useState } from 'react'; 
 export function Messages(){
+    const [message, setMessage] = useState('');
+    function sendMessage(event){ 
+        alert(message); 
+    }
     return( 
         <main>
     <footer id="navigationList">   
       <input type="text" placeholder="Websocket Data" />
       <br />   
-      <textarea class="messages"> 
+      <textarea class="messages" onChange={(e) => setMessage(e.target.value)}>  
       </textarea>   
       <br /> 
-      <button type="button">Send Message</button>   
+      <input type="text" placeholder="Enter Username To Send" />  
+      <br /> 
+      <button type="button" onClick={sendMessage}>Send Message</button>    
     </footer> 
     </main>
     );
