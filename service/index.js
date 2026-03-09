f25233c5e647b8c5b80f7f4e5110719ea1055b39
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-03-09 16:50:06 -0600

    Added user create function

--- service/index.js
+++ service/index.js
@@ -11,6 +11,27 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter); 
  
+
+//create user 
+async function createUser(username, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    username: username,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user); 
+
+  return user;
+}
+
+//find a user by whatever value we want (this can be an email, or a username)
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
 app.listen(port, () => {  
   console.log(`Listening on port ${port}`);
 });
