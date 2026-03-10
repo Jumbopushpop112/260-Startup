fc0f2406150ac99c989b35ab3d31c6ccb64d1634
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-03-10 17:40:11 -0600

    Added call to user ip

--- src/faq/faq.jsx
+++ src/faq/faq.jsx
@@ -3,12 +3,13 @@ import { useState,useEffect} from "react";
 export function Faq(){
     const [quote, setQuote] = useState("");
     const [author, setAuthor] = useState("");
+    const [ip, setIp] = useState(null);
   useEffect(() => { 
     async function loadQuote() { 
       try{
-         const response = await fetch("https://api.quotable.io/random");
+         const response = await fetch("https://dummyjson.com/quotes/random");
          const data = await response.json(); 
-         setQuote(data.content);  
+         setQuote(data.quote);   
          setAuthor(data.author);  
       }
       catch(err){
@@ -19,6 +20,20 @@ export function Faq(){
     }
     loadQuote();
   }, []);
+  useEffect(() => { 
+    async function getIp() { 
+      try{
+         const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      setIp(data.ip); 
+      }
+      catch(err){
+        console.log(err);
+        setIp("0.0")
+      } 
+    }
+    getIp();
+  }, []);
       
     return(   
         <main> 
@@ -26,7 +41,8 @@ export function Faq(){
             <li className="FAQ">Is Slime Free? Yes, absolutely. All we ask is that you are kind to others</li>
             <li className="FAQ">What makes Slime special? Slime was developed by Matthew Hart, a CS student at BYU. It's his first project!</li>
             <li className="FAQ">Is it possible to take breaks from slime? Yes.</li>
-            <li className="FAQ">Random Quote: {quote} - {author}</li>      
+            <li className="FAQ">Random Quote: {quote} - {author}</li>   
+            <li className="FAQ">IP Address: {ip}</li>   
         </menu>     
         </main> 
     );
