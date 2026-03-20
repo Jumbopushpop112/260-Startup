b92675cf09b401886586db273bda590229c9f016
Matthew Hart <68395720+Jumbopushpop112@users.noreply.github.com>
2026-03-20 17:56:02 -0600

    Working on database

--- service/database.js
+++ service/database.js
@@ -1,1 +1,7 @@
-mongodb+srv://mhart:<db_password>@slimestartup.8wgs5rb.mongodb.net/?appName=SlimeStartup 
+//mongodb+srv://mhart:<db_password>@slimestartup.8wgs5rb.mongodb.net/?appName=SlimeStartup 
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('users'); 
